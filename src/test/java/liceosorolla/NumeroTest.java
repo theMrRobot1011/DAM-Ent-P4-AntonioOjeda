f42438e86698d1f5809a0f2e25060d35798ac4aa
DAM_1 <DAM_1@PB09>
2020-03-06 19:23:04 +0100

    base numero

--- src/test/java/liceosorolla/NumeroTest.java
+++ src/test/java/liceosorolla/NumeroTest.java
@@ -1,5 +1,12 @@
 package test.java.liceosorolla;
 
+import main.java.liceosorolla.Numero;
+import org.junit.Before;
+import org.junit.Test;
 public class NumeroTest {
-
+	private Numero numero;
+	@Before
+	public void Before() {
+		numero = new Numero(4,5);
+	}
 }
