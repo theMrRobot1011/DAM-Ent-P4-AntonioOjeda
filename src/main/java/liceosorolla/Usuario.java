109027a39bca3eb31ec585e5610efbd01b425d74
DAM_1 <DAM_1@PB10>
2020-03-06 20:05:14 +0100

    Completo

--- src/main/java/liceosorolla/Usuario.java
+++ src/main/java/liceosorolla/Usuario.java
@@ -1,5 +1,63 @@
 package main.java.liceosorolla;
 
-public class Usuario {
+import java.time.LocalDate;
+import java.time.Period;
 
+public class Usuario {
+	private String nombre;
+	private String apellidos;
+	private LocalDate fecha;
+	private int ident;
+	
+	
+	
+	public Usuario(String nombre, String apellidos, LocalDate fecha, int ident ) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fecha = fecha;
+		this.ident = ident;
+	}
+	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	public int getident() {
+		return ident;
+	}
+
+	public void setident(int ident) {
+		this.ident = ident;
+	}
+	
+	public boolean mayoriaEdad() {
+		boolean bool=true;
+		LocalDate ahora = LocalDate.now();
+		Period periodo = Period.between(fecha, ahora);
+		bool=18<periodo.getYears();
+		return bool;
+	}
 }
+
+
