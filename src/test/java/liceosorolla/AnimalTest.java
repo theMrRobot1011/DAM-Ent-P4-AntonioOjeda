f68d486319eb6c789b30775460f7074c365c0baa
DAM_1 <DAM_1@PB07>
2020-03-06 21:04:14 +0100

    animal

--- src/test/java/liceosorolla/AnimalTest.java
+++ src/test/java/liceosorolla/AnimalTest.java
@@ -1,5 +1,42 @@
 package test.java.liceosorolla;
 
-public class AnimalTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.liceosorolla.Animal;
+
+public class AnimalTest {
+	
+	private Animal animal;
+	@Before
+	public void Before() {
+		animal = new Animal("Anfivio","Dardo",20,30,"verde");
+	}
+	
+	@Test
+	public void comprobarEspecie() {
+		assertEquals("Anfivio", animal.getEspecie());
+	}
+	
+	@Test
+	public void comprobarRaza() {
+		assertEquals("Dardo", animal.getRaza());
+	}
+	
+	@Test
+	public void comprobarPeso() {
+		assertEquals(20, animal.getPeso());
+	}
+	
+	@Test
+	public void comprobarEdad() {
+		assertEquals(30, animal.getEdad());
+	}
+	
+	@Test
+	public void comprobarColorDePelo() {
+		assertEquals("verde", animal.getColorDePelo());
+	}
 }
