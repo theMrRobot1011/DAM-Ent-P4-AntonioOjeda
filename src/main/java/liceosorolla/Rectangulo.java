1f3a0ede362ff22fd6c88bb3b2023a05e455642c
Familia Ojeda <Familia Ojeda@DESKTOP-5UIP68O>
2020-03-25 15:06:50 +0100

    vfdssaf

--- src/main/java/liceosorolla/Rectangulo.java
+++ src/main/java/liceosorolla/Rectangulo.java
@@ -1,5 +1,62 @@
 package main.java.liceosorolla;
 
 public class Rectangulo {
-
+	private int lado1;
+	private int lado2;
+	
+	public Rectangulo(int lado1, int lado2) {
+		this.lado1 = lado1;
+		this.lado2 = lado2;
+	}
+	
+	
+	public int getLado1() {
+		return lado1;
+	}
+	public void setLado1(int lado1) {
+		this.lado1 = lado1;
+	}
+	public int getLado2() {
+		return lado2;
+	}
+	public void setLado2(int lado2) {
+		this.lado2 = lado2;
+	}
+	
+	
+	public boolean esCuadrado() {
+		boolean bool=false;
+		bool=lado1==lado2;
+		return bool;
+	}
+	
+	
+	public int perimetro() {
+		int perimetro=lado1*2+lado2*2;
+		return perimetro;
+	}
+	
+	
+	public int area() {
+		int area=lado1*lado2;
+		return area;
+	}
+	
+	public int hipotenusa() {
+		int hipo=(int) Math.sqrt((Math.pow(lado1, 2)+Math.pow(lado2, 2)));
+		return hipo;
+	}
+	
+	public int radioCircunferencia() {
+		int ladoMenor=lado1;
+		if(lado2<lado1) {
+			ladoMenor=lado2;
+		}
+		int radio=ladoMenor/2;
+		return radio;
+	}
+	
 }
+	
+
+
