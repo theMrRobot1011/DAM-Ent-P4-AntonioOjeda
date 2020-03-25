eec690d7cfbce0a59e74a8580cbec6c73f6a2644
Familia Ojeda <Familia Ojeda@DESKTOP-5UIP68O>
2020-03-25 15:07:48 +0100

    gawrghas

--- src/test/java/liceosorolla/RectanguloTest.java
+++ src/test/java/liceosorolla/RectanguloTest.java
@@ -1,5 +1,42 @@
 package test.java.liceosorolla;
 
-public class RectanguloTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+import main.java.liceosorolla.Rectangulo;
+
+public class RectanguloTest {
+	private Rectangulo rectangulo;
+	
+	@Before
+	public void Before() {
+		rectangulo = new Rectangulo(6,9);
+	}
+	
+	@Test
+	public void comprobarLados() {
+		boolean comprobar=false;
+		assertEquals(comprobar,rectangulo.esCuadrado());
+	}
+	
+	@Test
+	public void comprobarPerimetro() {
+		assertEquals(30,rectangulo.perimetro());
+	}
+	
+	@Test
+	public void comprobarArea() {
+		assertEquals(54,rectangulo.area());
+	}
+	
+	@Test
+	public void comprobarHipotenusa() {
+		assertEquals(10,rectangulo.hipotenusa());
+	}
+	
+	@Test
+	public void comprobarRadioCircunferencia() {
+		assertEquals(3,rectangulo.radioCircunferencia());
+	}
 }
